5bbedae30ac854a7157d7c1febd5053771945441
Geert Guldentops <geert.guldentops@ipc.be>
2016-08-02 13:39:09 +0200

    Added the trivia game lab.

---
+++ src/test/java/be/jidoka/clean/code/labs/trivia/uglytrivia/GameTest.java
@@ -0,0 +1,14 @@
+package be.jidoka.clean.code.labs.trivia.uglytrivia;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class GameTest {
+
+    @Test
+    public void true_is_true() throws Exception {
+        assertThat(false).isTrue();
+    }
+
+}
