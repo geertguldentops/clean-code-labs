97ba2e4496688cfcb101a5933b2910531af768d3
Geert Guldentops <geert.guldentops@ipc.be>
2018-07-04 07:35:43 +0200

    Migrate to JUnit 5 and the latest versions of mockito and assertJ.

--- src/test/java/be/jidoka/clean/code/labs/trivia/uglytrivia/GameTest.java
+++ src/test/java/be/jidoka/clean/code/labs/trivia/uglytrivia/GameTest.java
@@ -1,13 +1,13 @@
 package be.jidoka.clean.code.labs.trivia.uglytrivia;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class GameTest {
+class GameTest {
 
     @Test
-    public void true_is_true() throws Exception {
+    void true_is_true() {
         assertThat(false).isTrue();
     }
 
