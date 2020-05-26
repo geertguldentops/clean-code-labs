432b59a5f901a7714686d4d2a537c343c1beba06
Geert Guldentops <geert.guldentops@corsearch.com>
2020-05-26 15:01:37 +0200

    Group TDD Clean Code 2020 (Java 8)

---
+++ src/test/java/be/jidoka/clean/code/labs/movie/BillTest.java
@@ -0,0 +1,76 @@
+package be.jidoka.clean.code.labs.movie;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.DayOfWeek;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BillTest {
+
+	private Bill bill;
+
+	@Before
+	public void setUp() {
+		bill = new Calculator();
+	}
+
+	@Test
+	public void generalAdmissionShouldEqualDefaultPrice() {
+		// Arrange
+		bill.startPurchase(100, DayOfWeek.WEDNESDAY, false, false);
+		bill.addTicket(18, false);
+
+		// Act
+		double ticketPrice = bill.finishPurchase();
+
+		// Assert
+		assertThat(ticketPrice).isEqualTo(11.0);
+	}
+
+	@Test
+	public void studentAdmissionShouldEqualStudentPrice() {
+		bill.startPurchase(100, DayOfWeek.WEDNESDAY, false, false);
+		bill.addTicket(18, true);
+
+		double ticketPrice = bill.finishPurchase();
+
+		assertThat(ticketPrice).isEqualTo(8.0);
+	}
+
+	@Test
+	public void multipleGeneralAdmissionsShouldEqualDefaultPrice() {
+		// Arrange
+		bill.startPurchase(100, DayOfWeek.WEDNESDAY, false, false);
+		bill.addTicket(18, false);
+		bill.addTicket(18, false);
+
+		// Act
+		double ticketPrice = bill.finishPurchase();
+
+		// Assert
+		assertThat(ticketPrice).isEqualTo(22.0);
+	}
+
+	@Test
+	public void _3DShouldAddToTicketPrice() {
+		bill.startPurchase(100, DayOfWeek.WEDNESDAY, false, true);
+		bill.addTicket(18, false);
+
+		double ticketPrice = bill.finishPurchase();
+
+		assertThat(ticketPrice).isEqualTo(14.0);
+	}
+
+	// ZOMBIE
+
+	// Zero
+	// One /
+	// Many /
+
+	// Boundaries
+	// Interface /
+	// Exception
+
+}
