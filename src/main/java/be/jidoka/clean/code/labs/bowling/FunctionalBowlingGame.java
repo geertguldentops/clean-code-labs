956c0c739ea53668b661b265ffddc87684474d61
Geert Guldentops <geertguldentops@jidoka.be>
2022-11-14 20:41:55 +0100

    Add a functional programming style solution for the bowling game kata

---
+++ src/main/java/be/jidoka/clean/code/labs/bowling/FunctionalBowlingGame.java
@@ -0,0 +1,69 @@
+package be.jidoka.clean.code.labs.bowling;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+class FunctionalBowlingGame {
+
+	private static final int MAX_NUMBER_OF_FRAMES_IN_SINGLE_GAME = 10;
+
+	private FunctionalBowlingGame() {
+		// No need to instantiate
+	}
+
+	public static int calculateScore(List<Integer> rolls) {
+		return frames(rolls).stream()
+				.limit(MAX_NUMBER_OF_FRAMES_IN_SINGLE_GAME)
+				.flatMap(Collection::stream)
+				.mapToInt(Integer::intValue)
+				.sum();
+	}
+
+	public static List<List<Integer>> frames(List<Integer> rolls) {
+		if (rolls.isEmpty()) {
+			return new ArrayList<>();
+		} else {
+			var partitions = new ArrayList<List<Integer>>();
+
+			if (isStrike(rolls)) {
+				partitions.add(take(rolls, 3));
+				partitions.addAll(frames(drop(rolls, 1)));
+			} else if (isSpare(rolls)) {
+				partitions.add(take(rolls, 3));
+				partitions.addAll(frames(drop(rolls, 2)));
+			} else {
+				partitions.add(take(rolls, 2));
+				partitions.addAll(frames(drop(rolls, 2)));
+			}
+
+			return partitions;
+		}
+	}
+
+	private static List<Integer> take(List<Integer> rolls, int n) {
+		var subset = new ArrayList<Integer>();
+
+		for (int i = 0; i < n; i++) {
+			if (i < rolls.size()) {
+				subset.add(rolls.get(i));
+			}
+		}
+
+		return subset;
+	}
+
+	private static List<Integer> drop(List<Integer> rolls, int n) {
+		return rolls.subList(n, rolls.size());
+	}
+
+	private static boolean isStrike(List<Integer> rolls) {
+		return rolls.get(0) == 10;
+	}
+
+	private static boolean isSpare(List<Integer> rolls) {
+		return rolls.get(0) + rolls.get(1) == 10;
+	}
+
+}
---
+++ src/test/java/be/jidoka/clean/code/labs/bowling/FunctionalBowlingGameTest.java
@@ -0,0 +1,93 @@
+package be.jidoka.clean.code.labs.bowling;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static be.jidoka.clean.code.labs.bowling.FunctionalBowlingGame.calculateScore;
+import static be.jidoka.clean.code.labs.bowling.FunctionalBowlingGame.frames;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FunctionalBowlingGameTest {
+
+	@Nested
+	class Scoring {
+
+		@Test
+		void all_gutter_rolls() {
+			assertThat(calculateScore(range(0, 20).map(operand -> 0).boxed().collect(toList()))).isZero();
+		}
+
+		@Test
+		void all_normal_rolls() {
+			assertThat(calculateScore(range(0, 20).map(operand -> 3).boxed().collect(toList()))).isEqualTo(60);
+		}
+
+		@Test
+		void one_spare() {
+			assertThat(calculateScore(List.of(5, 5, 2, 1))).isEqualTo(15);
+		}
+
+		@Test
+		void one_strike() {
+			assertThat(calculateScore(List.of(10, 3, 2, 1, 0))).isEqualTo(21);
+		}
+
+		@Test
+		void perfect_game() {
+			assertThat(calculateScore(range(0, 12).map(operand -> 10).boxed().collect(toList()))).isEqualTo(300);
+		}
+
+	}
+
+	@Nested
+	class FramePartitioning {
+
+		@Test
+		void partitions_no_frames() {
+			assertThat(frames(Collections.emptyList())).isEmpty();
+		}
+
+		@Test
+		void partitions_normal_frames() {
+			assertThat(frames(List.of(0, 0, 1, 1))).isEqualTo(List.of(List.of(0, 0), List.of(1, 1)));
+		}
+
+		@Test
+		void partitions_spare_frames() {
+			assertThat(frames(List.of(5, 5, 2, 1))).isEqualTo(List.of(List.of(5, 5, 2), List.of(2, 1)));
+		}
+
+		@Test
+		void partitions_strike_frames() {
+			assertThat(frames(List.of(10, 3, 2, 1, 0))).isEqualTo(List.of(List.of(10, 3, 2), List.of(3, 2), List.of(1, 0)));
+		}
+
+		@Test
+		void partitions_all_strike_frames() {
+			assertThat(frames(range(0, 12).map(operand -> 10).boxed().collect(toList())))
+					.isEqualTo(
+							List.of(
+									List.of(10, 10, 10),
+									List.of(10, 10, 10),
+									List.of(10, 10, 10),
+									List.of(10, 10, 10),
+									List.of(10, 10, 10),
+									List.of(10, 10, 10),
+									List.of(10, 10, 10),
+									List.of(10, 10, 10),
+									List.of(10, 10, 10),
+									List.of(10, 10, 10),
+									List.of(10, 10),
+									List.of(10)
+							)
+					);
+		}
+
+	}
+
+}
